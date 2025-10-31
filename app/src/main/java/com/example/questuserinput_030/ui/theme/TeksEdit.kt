07551e9974e655a8217eb79ca6d8e4c610faf283
gagatpadi <padiyogaradhana@gmail.com>
2025-10-31 15:06:26 +0700

    val gender

--- app/src/main/java/com/example/questuserinput_030/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_030/ui/theme/TeksEdit.kt
@@ -1,12 +1,21 @@
 package com.example.questuserinput_030.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Outline
 
 @Composable
-fun FormDataDiri(modifier: Modifier)
+fun FormDataDiri(modifier: Modifier
 ){
     //variabel-variabel untuk mengingat nilai masukan dari keyboard
     var textNama by remember { mutableStateOf(value = "")}
@@ -17,5 +26,23 @@ fun FormDataDiri(modifier: Modifier)
     var nama by remember {mutableStateOf(value = "")}
     var alamat by remember {mutableStateOf(value = "")}
     var jenis by remember {mutableStateOf(value = "")}
+
+    val gender:Listof("Laki laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(width = 250.dp),
+            label = { Text(text = "Nama Lengkap")},
+            onValueChange = {
+                textNama = it
+            }
+        )
+
+    }
 
 }
